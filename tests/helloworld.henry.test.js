dfc220aac4d506e05e873a7034a7f1f46085353d
Henry Ohia <Henry Ohia>
2023-10-20 05:11:14 +0100

    added deleted files

--- tests/helloworld.henry.test.js
+++
@@ -1,9 +0,0 @@
-import sayHello from '../utils/helloworld.glen.js';
-import assert from "assert";
-
-it('Testing Henry Hello', () => {
-    const hello = sayHello();
-
-    assert.equal(hello,'Henry')
-    
-});
--- utils/helloworld.henry.js
+++
@@ -1,5 +0,0 @@
-const sayHello = () => {
-  return "Henry";
-};
-
-export default sayHello;
